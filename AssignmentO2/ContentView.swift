89c06ddda53967cc59b53c6f699a4819ce7ab985
ZKMax01 <gitproject@zoltankoos.eu>
2025-04-13 09:33:11 +0200

    Initial changes

--- AssignmentO2/ContentView.swift
+++ AssignmentO2/ContentView.swift
@@ -6,16 +6,45 @@
 //
 
 import SwiftUI
+import DesignKit
 
 struct ContentView: View {
+    
+    @State private var username: String = ""
+    @State private var password: String = ""
+    
+    let standardStyle = ImputStyle(
+        borderColor: Color.App.Content.OnNeutral.danger,
+        textColor: .red,
+        placeholderColor: .orange,
+        validationMessageColor: .blue,
+        font: Font.App.Body.m,
+        padding: 20,
+        borderWidth: 4,
+        cornerRadius: 16
+    )
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: Styling.spacingM) {
+            VStack(alignment: .leading) {
+                Text("Username")
+                    .applyLabelSStyle()
+                TextImput(text: $username, placeholder: "Username")
+            }
+            VStack(alignment: .leading) {
+                Text("Password")
+                    .applyLabelSStyle()
+                PasswordImput(
+                    text: $password,
+                    placeholder: "Password",
+                    passwordMinLength: 8,
+                    validationCharacterMessage: "Password must be at least \(8) characters long.",
+                    validationSymbolMessage: "Password must include at least one uppercase letter and one symbol."
+                )
+            }
         }
         .padding()
+        .environment(\.imputStyle, ImputStyle())
     }
 }
 
