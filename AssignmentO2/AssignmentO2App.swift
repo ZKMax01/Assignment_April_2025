f9290535dea77c3620a649f9f252f89d584cbbe1
ZKMax01 <gitproject@zoltankoos.eu>
2025-04-13 09:00:00 +0200

    Initial Commit

---
+++ AssignmentO2/AssignmentO2App.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentO2App.swift
+//  AssignmentO2
+//
+//  Created by ZKMax01 on 13/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentO2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AssignmentO2Tests/AssignmentO2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentO2Tests.swift
+//  AssignmentO2Tests
+//
+//  Created by ZKMax01 on 13/04/2025.
+//
+
+import Testing
+@testable import AssignmentO2
+
+struct AssignmentO2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
